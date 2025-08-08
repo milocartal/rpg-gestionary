8c6e0f6851f017a9eda234688ade8ec0d27f4039
Milo CARTAL <milo.cartal.pro@gmail.com>
2025-08-08 22:15:40 +0200

    fix: change transporter

--- src/lib/mailer.ts
+++ src/lib/mailer.ts
@@ -12,7 +12,9 @@ export interface EmailDetails {
 }
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: env.GOOGLE_APP_USER,
     pass: env.GOOGLE_APP_PASSWORD,
